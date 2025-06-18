c2f8d38fedc6fe867825e3fee3c7b7f786f309d2
Gravyie <gappisingh08@gmail.com>
2025-06-18 15:02:11 +0530

    checking

--- src/components/ComponentModel.jsx
+++ src/components/ComponentModel.jsx
@@ -4,13 +4,13 @@ import { Canvas } from "@react-three/fiber";
 
 function Model({ i }) {
     const {scene} = useGLTF(`/models/Model${i}.glb`);
-    return <primitive object={scene} position={[0, -1, 0]} />;
+    return <primitive object={scene} position={[0, -8, 0]} />;
 }
 
 export default function ComponentModel({i, bgColor}) {
     return (
         <div className={`w-[60%] md:w-[70%] h-[150px] md:h-[350px] rounded-2xl overflow-hidden shadow-lg ${bgColor}`}>
-            <Canvas camera={{ position: [10, 10, -10], fov: 40}}>
+            <Canvas camera={{ position: [90, 30, -50], fov: 40}}>
                 <ambientLight intensity={10} />
                 <directionalLight position={[10, 10, 10]} intensity={10} />
                 <OrbitControls enableZoom={true} />
