177c5d3a428f8146b915d63d0f82056383547a90
Yash Gupta <yashgupta@spinfluencemedia.com>
2025-06-29 13:03:34 +0530

    sefuhseu

--- src/components/Hero3d.jsx
+++ src/components/Hero3d.jsx
@@ -14,16 +14,16 @@ export default function Hero3d() {
   return (
     <div className="relative w-full h-[92vh] overflow-hidden">
       {/* Background Video */}
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
-      >
-        <source src="/bg-video2.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        >
+          <source src="/bg-video3.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       <div className="relative flex flex-rows z-10">
         <motion.div
           initial={{ x: -50, opacity: 0 }}
